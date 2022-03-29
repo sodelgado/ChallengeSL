2f81fc37d0c7c4274f6a4810d159f2f8337db332
Sofia Delgado <sofiab.delgado@mercadolibre.com>
2022-03-29 14:39:52 -0300

    Initial Commit

---
+++ ChallengeSL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeSL
+//
+//  Created by Sofia Belen Delgado Gonzalez on 29/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
