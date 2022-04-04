6a210efceb5018ffb7d24be5b74f4678665f2353
Sofia Delgado <sofiab.delgado@mercadolibre.com>
2022-04-04 13:44:30 -0300

    Add Models and class DataService

--- ChallengeSL/DataService.swift
+++ ChallengeSL/DataService.swift
@@ -8,57 +8,78 @@
 import Foundation
 import Alamofire
 
-
-
-func getCategoryID(textValue: String) {
-    let categoryURL: String = ("https://api.mercadolibre.com/sites/MLA/domain_discovery/search?q=\(textValue)")
-  let request = AF.request(categoryURL)
-    request.responseDecodable(of: [CategoryPredictor].self) { (response) in
-      guard let category = response.value else { return }
-        print(category[0].categoryID)
-    }
-
-}
-
-//func getHighlights(categoryID: String, completion: @escaping (Result<[CategoryPredictor],NetworkError>)-> Void) {
-//    let highlightsURL: String = "https://api.mercadolibre.com/highlights/MLA/category/\(categoryID)"
-//        AF.request(highlightsURL, method: .get).responseJSON { response in
-//            guard let itemsData = response.data else {
-//                completion(.failure(.noData))
-//              return
-//            }
-//            do {
-//                let decoder = JSONDecoder()
-//                let items = try decoder.decode([CategoryPredictor].self, from: itemsData)
-//                DispatchQueue.main.async {
-//                    completion(.success(items))
-//                    print(items)
-//                }
-//            } catch {
-//                completion(.failure(.badDecodable))
-//            }
-//        }
-//    }
-
-//func getData(q: String,  completion: @escaping (Result<[CategoryPredictor], NetworkError>) -> Void) {
-//    let request = AF.request(categoryURL)
+//func getCategoryID(textValue: String) {
+//    let categoryURL: String = ("https://api.mercadolibre.com/sites/MLA/domain_discovery/search?q=\(textValue)")
+//  let request = AF.request(categoryURL)
 //    request.responseDecodable(of: [CategoryPredictor].self) { (response) in
-//        guard let categoryData = response.value else {
-//            return completion(.failure(.noData))
-//        }
-//        print(categoryData)
-//        return completion(.success(categoryData))
-//    }
-//}
-
-
-
-enum NetworkError: Error {
-    case noData
-    case badDecodable
+//      guard let category = response.value else { return }
+//        print(category[0].categoryID)
+        
+class DataService {
+    let header: HTTPHeaders = [.authorization(bearerToken: "APP_USR-2357420951050600-040416-2b97ea31724d7f2796a20721f4610bdd-251000745")]
+    var highlightsID: [String] = []
+    var firstCategoryID: String = ""
+    var arrayCategory = [CategoryPredictor]()
+    var vc = SearchViewController()
+    
+    func getCategoryID(textValue: String, completion: @escaping (Result<[CategoryPredictor], NetworkError>)-> Void){
+        let categoryURL: String = ("https://api.mercadolibre.com/sites/MLA/domain_discovery/search?q=\(textValue)")
+        let request = AF.request(categoryURL, headers: header)
+        request.responseDecodable(of: [CategoryPredictor].self) { (response) in
+            guard let category = response.value else {
+                return completion(.failure(.noData))
+            }
+            print(category[0].categoryID)
+            //        firstCategoryID = category[0].categoryID
+            //        print(firstCategoryID)
+            return completion(.success(category))
+        }
+    }
+    
+    func getCategoryID2(textValue: String){
+        let categoryURL: String = ("https://api.mercadolibre.com/sites/MLA/domain_discovery/search?q=\(textValue)")
+        AF.request(categoryURL, headers: header).response { (response) in
+            if let data = response.data {
+                do{
+                    self.arrayCategory = try JSONDecoder().decode([CategoryPredictor].self, from: data)
+                    print(self.arrayCategory[0].categoryID)
+                } catch let err {
+                    print(NetworkError.noData)
+                }
+            }
+        }
+    }
+    
+    func getHighlights(categoryID: String, completion: @escaping (Result<Highlights, NetworkError>) -> Void) {
+        let highlightsURL: String = ("https://api.mercadolibre.com/highlights/MLA/category/\(categoryID)")
+        let request = AF.request(highlightsURL, headers: header)
+        request.responseDecodable(of: Highlights.self) { (response) in
+            guard let data = response.value else {
+                print("no anda")
+                return completion(.failure(.noData))
+            }
+            print(data)
+            //        highlightsID = data.content.map({ $0.id })
+            print(self.highlightsID)
+            print("jelou")
+            return completion(.success(data))
+        }
+    }
+    
+    func multiget(array: [String], completion: @escaping (Result<[Top20Element], NetworkError>) -> Void) {
+        let top20URL: String = ("https://api.mercadolibre.com/items?ids=\(highlightsID)&attributes=id,price,pictures")
+        let request = AF.request(top20URL, headers: header)
+        request.responseDecodable(of: [Top20Element].self) { (response) in
+            guard let data = response.value else {
+                return completion(.failure(.noData))
+            }
+            print(data)
+            print(data[0].body.title)
+            return completion(.success(data))
+        }
+    }
+    
+    enum NetworkError: Error {
+        case noData
+    }
 }
-
-
-
-
-
---
+++ ChallengeSL/Model/MultigetModel.swift
@@ -0,0 +1,37 @@
+//
+//  MultigetModel.swift
+//  ChallengeSL
+//
+//  Created by Sofia Belen Delgado Gonzalez on 4/4/22.
+//
+
+import Foundation
+// MARK: - Top20Element
+struct Top20Element: Codable {
+    let code: Int
+    let body: Body
+}
+
+// MARK: - Body
+struct Body: Codable {
+    let pictures: [Picture]
+    let price: Double
+    let id, title: String
+}
+
+// MARK: - Picture
+struct Picture: Codable {
+    let quality, id: String
+    let url: String
+    let secureURL: String
+    let size, maxSize: String
+
+    enum CodingKeys: String, CodingKey {
+        case quality, id, url
+        case secureURL = "secure_url"
+        case size
+        case maxSize = "max_size"
+    }
+}
+
+typealias Top20 = [Top20Element]
